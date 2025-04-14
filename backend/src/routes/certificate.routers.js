7652605a62d1b55a00ef7772de8fc16311d33306
sreegopalsaha <sreegopal0101@gmail.com>
2025-04-14 17:31:27 +0530

    fix some errors

--- backend/src/routes/certificate.routers.js
+++ backend/src/routes/certificate.routers.js
@@ -1,11 +1,12 @@
 import express from 'express';
 const router = express.Router();
+import checkAuth from "../middlewares/checkAuth.js";
 
 import {
     createCertificate, getCertificate
-} from "../controllers/certificate.controller.js";
+} from "../controllers/certificate.controllers.js";
 
-router.post("/create", createCertificate);
-router.get("/:certificateId", getCertificate);
+router.post("/create", checkAuth, createCertificate);
+router.get("/get/:certificateId", getCertificate);
 
 export default router;
