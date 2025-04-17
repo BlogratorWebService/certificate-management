95f2e018a83410ec18c7ffcfc91e637ef354fab9
sreegopalsaha <sreegopal0101@gmail.com>
2025-04-17 23:50:32 +0530

    feat: implement authentication with PrivateRoute and useAuth hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,19 +4,23 @@ import Dashboard from "./pages/dashboard/page";
 import Login from "./pages/login/page";
 import NewStudent from "./pages/new/page";
 import StudentPage from "./pages/student/page";
+import PrivateRoute from "./components/PrivateRoutes";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
-
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Dashboard />} />
-          <Route element={<NewStudent />} path="/new" />
-          <Route
-            path="/student/:registrationNumber" element={<StudentPage />}
-          />
+        
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="/new" element={<NewStudent />} />
+            <Route
+              path="/student/:registrationNumber"
+              element={<StudentPage />}
+            />
+          </Route>
         </Route>
       </Routes>
     </Router>
