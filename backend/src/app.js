35eaa7bcad29fc8f71bf370fb11462933028e97b
sreegopalsaha <sreegopal0101@gmail.com>
2025-04-21 20:09:22 +0530

    serve file from public dir not cloudinary

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,6 +18,8 @@ app.use(cookieParser());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.send("Hello from the backend server!");
 });
